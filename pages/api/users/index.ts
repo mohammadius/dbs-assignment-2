1eefd62bcec7d40e25a012ae713830fc18377296
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-15 21:27:49 +0330

    fixed

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,12 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "utils/mongodb";
-import { Gender, User } from "models";
+import { User } from "models";
 
 export default async (req: NextApiRequest, res: NextApiResponse<User[] | User>) => {
-	const {
-		// query: { id, name },
-		method
-	} = req;
+	const { method } = req;
 	switch (method) {
 		case "GET":
 			try {
@@ -18,10 +15,9 @@ export default async (req: NextApiRequest, res: NextApiResponse<User[] | User>) 
 				res.status(500).json(error);
 			}
 			break;
-		case "POST":
-			// TODO: create user
-			res.status(200).json(new User("", "", new Date(), "", "", Gender.Male));
-			break;
+		// case "POST":
+		// TODO: create user
+		// break;
 		default:
 			res.setHeader("Allow", ["GET", "POST"]);
 			res.status(405).end(`Method ${method} Not Allowed`);
