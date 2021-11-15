c6f54b9837f3bdbe857c898cae8362cc7e9e1994
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-15 21:21:01 +0330

    added user model

---
+++ models/user.ts
@@ -0,0 +1,14 @@
+import type { ObjectId } from "mongodb";
+import type { Gender } from "models";
+
+export class User {
+	constructor(
+		public firstName: string,
+		public lastName: string,
+		public birthDate: Date,
+		public address: string,
+		public phoneNumber: string,
+		public gender: Gender,
+		public id?: ObjectId
+	) {}
+}
