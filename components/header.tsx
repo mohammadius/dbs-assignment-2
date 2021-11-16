c0b110bb756a7b4add9ea112f9a93c841367ff35
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-11-16 16:18:47 +0330

    better styles

--- components/header.tsx
+++ components/header.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 const Header = () => {
 	return (
-		<header className='w-full border-b mb-4'>
+		<header className='w-full border-b mb-4 shadow-md'>
 			<ul className='flex items-center justify-center h-20 gap-8'>
 				<li>
 					<Link href='/users'>
-						<a>Users</a>
+						<a className='text-base font-medium text-gray-500 hover:text-gray-900 transition-colors'>Users</a>
 					</Link>
 				</li>
 				<li>
 					<Link href='/users/add'>
-						<a>Add New User</a>
+						<a className='text-base font-medium text-gray-500 hover:text-gray-900 transition-colors'>Add New User</a>
 					</Link>
 				</li>
 			</ul>
--- components/indexLink.tsx
+++ components/indexLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const IndexLink = ({ href, text }) => {
 	return (
 		<Link href={href}>
-			<a className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600 transition-all shadow-lg hover:shadow-md focus:shadow-none'>
+			<a className='p-6 mt-6 text-left border hover:border-blue-200 w-96 rounded-xl hover:text-blue-600 focus:text-blue-600 transition-all shadow-lg hover:shadow-md focus:shadow-none'>
 				<h3 className='text-2xl font-bold'>{text}</h3>
 			</a>
 		</Link>
